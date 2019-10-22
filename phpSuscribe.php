f7c46a68922d8ee6dae14c737707874869eea4ad
Ezequiel <ezequiel.domenech@gmail.com>
2019-10-22 13:02:58 -0300

    correccciones respecto a la api que usa el plugin

--- phpSuscribe.php
+++ phpSuscribe.php
@@ -46,174 +46,282 @@ add_action('widgets_init', 'smu_widget');
  * Add any links to .css or .js files here
  */
 function smu_add_css_js_files(){
-	wp_enqueue_script('jQuery-Validation', plugins_url('/assets/js/lib/jquery.validation/1.13.1/jquery.validate.js', __FILE__), array('jquery'));
+	wp_enqueue_script('jQuery-Validation', plugins_url('/assets/js/lib/jquery.validation/1.19.1/jquery.validate.js', __FILE__), array('jquery'));
 	wp_enqueue_style( 'smu-widget-stylesheet', plugins_url('/assets/css/style.css', __FILE__), false, '1.0.0', 'all');
 }
 add_action('wp_enqueue_scripts', "smu_add_css_js_files");
 
-
-
-class smu_widget extends WP_Widget {
-
-	/**
-	 * smu_widget
-	 *
-	 * Setup the widget, register scripts etc
-	 *
-	 * @return void
-	 */
-	function __construct() {
-		$widget_ops = array( 'classname' => 'smu-widget', 'description' => __('A simple sign-up form that integrates with phpList', 'smu') );
-		parent::__construct( 'smu-widget', __('Sign Me Up', 'smu'), $widget_ops );
-
-		add_action( 'init', array( $this, 'smu_register_script' ) ) ;
-
-		// only load scripts when an instance is active
-		if ( is_active_widget( false, false, $this->id_base ) && !is_admin())
-			add_action( 'wp_footer', array( $this, 'smu_print_script' ) );
-
-		add_action('wp_ajax_smu', array( $this, 'smu_ajax') );
-		add_action('wp_ajax_nopriv_smu', array( $this, 'smu_ajax') );
-
-	}
-
-	/**
-	 * widget
-	 *
-	 * Output the widget
-	 *
-	 * @return void
-	 */
-	function widget($args, $instance) {
-
-		extract($args, EXTR_SKIP);
-		$title = empty($instance['title']) ? '' : apply_filters('widget_title', $instance['title']);
-		$descr = empty($instance['descr']) ? '' : esc_attr( $instance['descr'] );
-		$submit_url = esc_url( $instance['submit_url'] );
-		$placeholder = empty($instance['placeholder']) ? '' : esc_attr( $instance['placeholder'] );
-		$label = empty($instance['label']) ? '' : esc_attr( $instance['label'] );
-		$button_txt = empty($instance['button_txt']) ? '' : esc_attr( $instance['button_txt'] );
-		
-		echo $before_widget;
-		if (!empty($title)) {
-			echo '<h2>' . $before_title . $title . $after_title . '</h2>' . PHP_EOL; 
-			echo '<p>' . $descr . '</p>' . PHP_EOL;
-		};
-		
-		//$wait_image_url = 'https://s3.amazonaws.com/phplist/img/busy.gif';
-		$wait_image_url = plugins_url('/assets/images/busy.gif', __FILE__);
-		
-		?>
-		
-		<form class="smu-subscribe-form" method="post" action="<?php echo $submit_url; ?>">
-			<label for="email"><?php echo $label; ?></label>
-			<input type="email" id="smu_email" name="email" placeholder="<?php echo $placeholder; ?>" onfocus="smu_clearError()" required>
-			<input type="submit" class="submit" value="<?php echo $button_txt; ?>">
-			<img src="<?php echo $wait_image_url; ?>" class="wait-img" alt="Please wait" title="powered by phpList, www.phplist.com">
-		</form>
-		<div id="smu-jquery-error"></div>
-		<div class="smu-server-response"></div>
-		
-		<?php
-		echo $after_widget;
-
-	}
-
-
-	/**
-	 * form
-	 *
-	 * Edit widget form
-	 *
-	 * @return void
-	 */
-	function form($instance) {
-		$instance = wp_parse_args( (array) $instance, array( 'title' => __('Sign Me Up', 'smu'), 'descr' => __('', 'smu'), 'submit_url' => __('', 'smu'), 'placeholder' => __('', 'smu'), 'label' => __('', 'smu'), 'button_txt' => __('', 'smu') ) );
-		$title = esc_attr( $instance['title'] );
-		$descr = esc_attr( $instance['descr'] );
-		$submit_url = esc_url('submit_url');
-		$placeholder = esc_attr( $instance['placeholder'] );
-		$label = esc_attr( $instance['label'] );
-		$button_txt = esc_attr( $instance['button_txt'] );
-
-		?>
-		
-		<p>
-			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title', 'smu'); ?>:</label>
-			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $instance['title']; ?>" />
-		</p>
-		<p>
-			<label for="<?php echo $this->get_field_id('descr'); ?>"><?php _e('Text before form', 'smu'); ?>:</label>
-			<textarea class="widefat" id="<?php echo $this->get_field_id('descr'); ?>" name="<?php echo $this->get_field_name('descr'); ?>"><?php echo $instance['descr']; ?></textarea>
-		</p>
-		<p>
-			<label for="<?php echo $this->get_field_id('submit_url'); ?>"><?php _e('URL of phpList \'Subscribe Page\'', 'smu'); ?>:</label>
-			<input class="widefat" id="<?php echo $this->get_field_id('submit_url'); ?>" name="<?php echo $this->get_field_name('submit_url'); ?>" type="text" value="<?php echo $instance['submit_url']; ?>" />
-		</p>
-		<p>
-			<label for="<?php echo $this->get_field_id('placeholder'); ?>"><?php _e('Placeholder text (inside input box)', 'smu'); ?>:</label>
-			<input class="widefat" id="<?php echo $this->get_field_id('placeholder'); ?>" name="<?php echo $this->get_field_name('placeholder'); ?>" type="text" value="<?php echo $instance['placeholder']; ?>" />
-		</p>
-		<p>
-			<label for="<?php echo $this->get_field_id('label'); ?>"><?php _e('Label text (above input box)', 'smu'); ?>:</label>
-			<input class="widefat" id="<?php echo $this->get_field_id('label'); ?>" name="<?php echo $this->get_field_name('label'); ?>" type="text" value="<?php echo $instance['label']; ?>" />
-		</p>
-		<p>
-			<label for="<?php echo $this->get_field_id('button_txt'); ?>"><?php _e('Button text (e.g, "Subscribe")', 'smu'); ?>:</label>
-			<input class="widefat" id="<?php echo $this->get_field_id('button_txt'); ?>" name="<?php echo $this->get_field_name('button_txt'); ?>" type="text" value="<?php echo $instance['button_txt']; ?>" />
-		</p>
-		<?php
-	}
-	
-	
-	/**
-	 * update
-	 *
-	 * Save the new widget instance
-	 *
-	 * @return array
-	 */
-	function update($new_instance, $old_instance) {
-		$instance = $old_instance;
-		$instance['title'] = strip_tags($new_instance['title']);
-		$instance['descr'] = strip_tags($new_instance['descr']);
-		$instance['submit_url'] = esc_url($new_instance['submit_url']);
-		$instance['placeholder'] = strip_tags($new_instance['placeholder']);
-		$instance['label'] = strip_tags($new_instance['label']);
-		$instance['button_txt'] = strip_tags($new_instance['button_txt']);
-		return $instance;
-	}
-
-
-	/**
-	 * smu_register_script
-	 *
-	 * Register the JS for ajax request
-	 *
-	 * @return void
-	 */
-	function smu_register_script() {
-		wp_register_script( 'smu', plugins_url('/assets/js/magic.js', __FILE__), array('jquery'), '1.0', true);
-
-		wp_localize_script('smu','smu', array(
-			'ajax_url' => esc_url(add_query_arg(array('action' => 'smu','_wpnonce' => wp_create_nonce( 'smu' )), untrailingslashit(admin_url('admin-ajax.php')))),
-		));
-	}
-
-
-	/**
-	 * smu_print_script
-	 *
-	 * Output JS only when widget in use on page
-	 *
-	 * @return void
-	 */
-	function smu_print_script() {
-		wp_print_scripts( 'smu' );
-	}
-
-
-	
+class phpListRESTApiClient {
+    /**
+     * URL of the API to connect to including the path
+     * generally something like.
+     * 
+     * https://website.com/lists/admin/?pi=restapi&page=call
+     */
+    private $url;
+    /**
+     * login name for the phpList installation.
+     */
+    private $loginName;
+    /** 
+     * password to login.
+     */
+    private $password;
+    /**
+     * the path where we can write our cookiejar.
+     */
+    public $tmpPath = '/tmp';
+    /**
+     * optionally the remote processing secret of the phpList installation
+     * this will increase the security of the API calls.
+     */
+    private $remoteProcessingSecret;
+    /**
+     * construct, provide the Credentials for the API location.
+     * 
+     * @param string $url URL of the API
+     * @param string $loginName name to login with
+     * @param string $password password for the account
+     * @return null
+     */
+    public function __construct($url, $loginName, $password, $secret = '') {
+        $this->url = $url;
+        $this->loginName = $loginName;
+        $this->password = $password;
+        $this->remoteProcessingSecret = $secret;
+    }
+    /**
+     * Make a call to the API using cURL.
+     * 
+     * @param string $command The command to run
+     * @param array $post_params Array for parameters for the API call
+     * @param bool $decode json_decode the result (defaults to true)
+     *
+     * @return string result of the CURL execution
+     */
+    private function callApi($command, $post_params, $decode = true, $newSession = false) {
+        $post_params['cmd'] = $command;
+        // optionally add the secret to a call, if provided
+        if (!empty($this->remoteProcessingSecret)) {
+            $post_params['secret'] = $this->remoteProcessingSecret;
+        }
+        $post_params = http_build_query($post_params);
+        $c = curl_init();
+        curl_setopt($c, CURLOPT_URL,            $this->url);
+        curl_setopt($c, CURLOPT_HEADER,         0);
+        curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($c, CURLOPT_POST,           1);
+        curl_setopt($c, CURLOPT_POSTFIELDS,     $post_params);
+        curl_setopt($c, CURLOPT_COOKIEFILE,     $this->tmpPath.'/phpList_RESTAPI_cookiejar.txt');
+        curl_setopt($c, CURLOPT_COOKIEJAR,      $this->tmpPath.'/phpList_RESTAPI_cookiejar.txt');
+		curl_setopt($c, CURLOPT_COOKIESESSION,	$newSession); // Fix for random failed authentication
+        curl_setopt($c, CURLOPT_HTTPHEADER,     array('Connection: Keep-Alive', 'Keep-Alive: 60'));
+        // Execute the call
+        $result = curl_exec($c);
+        // Check if decoding of result is required
+        if ($decode === true) {
+            $result = json_decode($result);
+        }
+        return $result;
+    }
+    /**
+     * Use a real login to test login api call.
+     * 
+     * @param none
+     *
+     * @return bool true if user exists and login successful
+     */
+    public function login() {
+        // Set the username and pwd to login with
+        $post_params = array(
+            'login' => $this->loginName,
+            'password' => $this->password,
+        );
+        // Execute the login with the credentials as params
+        $result = $this->callApi('login', $post_params, true, true);
+        return $result->status == 'success';
+    }
+    /**
+     * Create a list.
+     * 
+     * @param string $listName        Name of the list
+     * @param string $listDescription Description of the list
+     *
+     * @return int ListId of the list created
+     */
+    public function listAdd($listName, $listDescription) {
+        // Create minimal params for api call
+        $post_params = array(
+            'name' => $listName,
+            'description' => $listDescription,
+            'listorder' => '0',
+            'active' => '1',
+        );
+        // Execute the api call
+        $result = $this->callAPI('listAdd', $post_params);
+        // get the ID of the list we just created
+        $listId = $result->data->id;
+        return $listId;
+    }
+    /**
+     * Get all lists.
+     *
+     * @return array All lists
+     */
+    public function listsGet() {
+        // Create minimal params for api call
+        $post_params = array();
+        // Execute the api call
+        $result = $this->callAPI('listsGet', $post_params);
+        // Return all list as array
+        return $result->data;
+    }
+    /**
+     * Find a subscriber by email address.
+     * 
+     * @param string $emailAddress Email address to search
+     *
+     * @return int $subscriberID if found false if not found
+     */
+    public function subscriberFindByEmail($emailAddress) {
+        $params = array('email' => $emailAddress,);
+        $result = $this->callAPI('subscriberGetByEmail', $params);
+        if (!empty($result->data->id)) {
+            return $result->data->id;
+        } else {
+            return false;
+        }
+    }
+    /**
+     * Add a subscriber.
+     * 
+     * This is the main method to use to add a subscriber. It will add the subscriber as 
+     * a non-confirmed subscriber in phpList and it will send the Request-for-confirmation
+     * email as set up in phpList.
+     * 
+     * The lists parameter will set the lists the subscriber will be added to. This has
+     * to be comma-separated list-IDs, eg "1,2,3,4".
+     * 
+     * @param string $emailAddress email address of the subscriber to add
+     * @param string $lists        comma-separated list of IDs of the lists to add the subscriber to
+     *
+     * @return int $subscriberId if added, or false if failed
+     */
+    public function subscribe($emailAddress, $lists) {
+        // Set the user details as parameters
+        $post_params = array(
+            'email' => $emailAddress,
+            'foreignkey' => '',
+            'htmlemail' => 1,
+            'subscribepage' => 0,
+            'lists' => $lists,
+        );
+        // Execute the api call
+        $result = $this->callAPI('subscribe', $post_params);
+        if (!empty($result->data->id)) {
+            $subscriberId = $result->data->id;
+            return $subscriberId;
+        } else {
+            return false;
+        }
+    }
+    /** 
+     * Fetch subscriber by ID.
+     *
+     * @param int $subscriberID ID of the subscriber
+     *
+     * @return the subscriber
+     */
+    public function subscriberGet($subscriberId) {
+        $post_params = array('id' => $subscriberId,);
+        // Execute the api call
+        $result = $this->callAPI('subscriberGet', $post_params);
+        if (!empty($result->data->id)) {
+            $fetchedSubscriberId = $result->data->id;
+            $this->assertEquals($fetchedSubscriberId, $subscriberId);
+            return $result->data;
+        } else {
+            return false;
+        }
+    }
+    /** 
+     * Get a subscriber by Foreign Key.
+     * 
+     * Note the difference with subscriberFindByEmail which only returns the SubscriberID
+     * Both API calls return the subscriber
+     * 
+     * @param string $foreignKey Foreign Key to search
+     *
+     * @return subscriber object if found false if not found
+     */
+    public function subscriberGetByForeignkey($foreignKey) {
+        $post_params = array('foreignkey' => $foreignKey,);
+        $result = $this->callAPI('subscriberGetByForeignkey', $post_params);
+        if (!empty($result->data->id)) {
+            return $result->data;
+        } else {
+            return false;
+        }
+    }
+     /**
+      * Get the total number of subscribers.
+      * 
+      * @param none
+      *
+      * @return int total number of subscribers in the system
+      */
+     public function subscriberCount() {
+         $post_params = array();
+         $result = $this->callAPI('subscribersCount', $post_params);
+         return $result->data->total;
+     }
+    /**
+     * Add a subscriber to an existing list.
+     * 
+     * @param int $listId       ID of the list 
+     * @param int $subscriberId ID of the subscriber
+     *
+     * @return the lists this subscriber is member of
+     */
+    public function listSubscriberAdd($listId, $subscriberId) {
+        // Set list and subscriber vars
+        $post_params = array(
+            'list_id' => $listId,
+            'subscriber_id' => $subscriberId,
+        );
+        $result = $this->callAPI('listSubscriberAdd', $post_params);
+        return $result->data;
+    }
+    /**
+     * Get the lists a subscriber is member of.
+     * 
+     * @param int $subscriberId ID of the subscriber
+     *
+     * @return the lists this subcriber is member of
+     */
+    public function listsSubscriber($subscriberId) {
+        $post_params = array(
+            'subscriber_id' => $subscriberId,
+        );
+        $result = $this->callAPI('listsSubscriber', $post_params);
+        return $result->data;
+    }
+    /**
+     * Remove a Subscriber from a list.
+     *
+     * @param int $listId       ID of the list to remove
+     * @param int $subscriberId ID of the subscriber
+     *
+     * @return the lists this subcriber is member of
+     */
+    public function listSubscriberDelete($listId, $subscriberId) {
+        // Set list and subscriber vars
+        $post_params = array(
+            'list_id' => $listId,
+            'subscriber_id' => $subscriberId,
+        );
+        $result = $this->callAPI('listSubscriberDelete', $post_params);
+        return $result->data;
+    }
 }
-
 ?>
